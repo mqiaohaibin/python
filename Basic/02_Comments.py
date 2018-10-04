aa5050216a4b27d5c77ed1be08c1e360180ed591
qiaohaibin <mqiaohaibin@gmail.com>
2018-10-04 17:44:19 +0800

    comment

---
+++ Basic/02_Comments.py
@@ -0,0 +1,38 @@
+"""
+In Python, we can add single-line and multi-line Python comment.
+
+Single-line comments begin with a pound (#) symbol and automatically
+ends with an EOL (end of the line).
+
+Multi-line comments are also known as block comments.
+
+Using The Hash (#) Mark
+To add multi-line comments, you should begin each line with the
+pound (#) symbol followed by a single space. You can divide a comment into
+paragraphs. Just add an empty line with a hash mark between each para.
+
+"""
+
+# Define a list of months
+months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+          'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+# Using The Hash (#) Mark
+#
+# To Learn any language you must follow the below rules.
+# 1. Know the basic syntax, data types, control structures and conditional statements.
+# 2. Learn error handling and file I/O.
+# 3. Read about advanced data structures.
+# 4. Write functions and follow OOPs concepts.
+
+'''
+Docstring In Python
+Python has the documentation strings (or docstrings) feature.
+You can define a docstring by adding it as a string constant. It must be the first 
+statement. You can define a docstring with the help of triple-quotation mark. Add one in 
+the beginning and second at the end of the string.
+
+The strings defined using triple-quotation mark are docstring in Python. However, it might 
+appear to you as a regular comment.
+'''
+
